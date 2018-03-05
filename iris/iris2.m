ce1d1f102cd4f71e128c4cbc63d4cca411192635
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-03-05 15:25:41 +0100

    Celanup lab1

--- iris/iris2.m
+++ iris/iris2.m
@@ -11,22 +11,14 @@ endfunction
 
 function retval = predict(data1, data2, point)
     % returns 1 or 2 (predicted class of point)
-    % d1 = distances(point, data1(:, 2:end));
-    % d2 = distances(point, data2(:, 2:end));
     d1 = distances(point, data1);
     d2 = distances(point, data2);
     [min1, i1] = min(d1);
     [min2, i2] = min(d2);
     % disp ('minimal distance:');
     if min1 < min2
-        % disp (min1);
-        % disp (data1(i1, :));
-        % disp ('class versicolor');
         retval = 1;
     else
-        % disp (min2);
-        % disp (data2(i2, :));
-        % disp ('class virginica');
         retval = 2;
     endif
 endfunction
@@ -37,7 +29,7 @@ function retval = getCorrect(class1, class2)
     % removing single elements from class1
     total = 0;
     for i = 1:size(class1)(1)
-        testSample = class1(i, :); % (2:end);
+        testSample = class1(i, :);
         trainData = class1(1:end != i, :);
         result = predict(trainData, class2, testSample);
         if result == 1
@@ -58,4 +50,25 @@ iris1 = load ('iris2.txt') (:, 2:end);
 iris2 = load ('iris3.txt') (:, 2:end);
 
 features = [1, 2, 3, 4];
-disp(evalForFeatures(iris1, iris2, features))
+disp('all fetures:')
+disp(evalForFeatures(iris1, iris2, 1:4))
+
+disp('---------3 fetures:')
+for i = 1:4
+    features = (1:4) (1:end != i)
+    disp(evalForFeatures(iris1, iris2, features));
+endfor
+
+disp('---------2 fetures:')
+for i = 1:4
+    for j = (i+1):4
+        features = (1:4) (1:end != i) (1:end != j-1)
+        disp(evalForFeatures(iris1, iris2, features));
+    endfor
+endfor
+
+disp('---------1 feature:')
+for i = 1:4
+    features = [i]
+    disp(evalForFeatures(iris1, iris2, features));
+endfor
