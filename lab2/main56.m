a4bb77c3ab1deb72381b24a9878c1aac9cd0024e
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-03-18 16:59:27 +0100

    Finish lab2

--- lab2/main56.m
+++ lab2/main56.m
@@ -16,44 +16,44 @@ test = filter_outliers(test);
 
 
 
-% disp('5. Parzen window width-----------------')
-% parts = (1:5) / 5;
-% parts = [0.06 0.08 parts]';
-% stat_means = [];
-% for i=1:rows(parts)
-%     experiments_count = 20;
-%     base_ercf = zeros(experiments_count,4);
-%     for r=1:experiments_count
-%         reduced = reduce(train, repmat(parts(i), rows(labels), 1));
-% 
-%         para1 = para_parzen(reduced, 0.0001); 
-%         para2 = para_parzen(reduced, 0.0005); 
-%         para3 = para_parzen(reduced, 0.001); 
-%         para4 = para_parzen(reduced, 0.002); 
-% 
-%         base_ercf(r, 1) = mean(bayescls(test(:,2:end), @pdf_parzen, para1) != test(:,1));
-%         base_ercf(r, 2) = mean(bayescls(test(:,2:end), @pdf_parzen, para2) != test(:,1));
-%         base_ercf(r, 3) = mean(bayescls(test(:,2:end), @pdf_parzen, para3) != test(:,1));
-%         base_ercf(r, 4) = mean(bayescls(test(:,2:end), @pdf_parzen, para4) != test(:,1));
-%     end
-%     stat_means = cat(1, stat_means, mean(base_ercf, 1));
-% end
-% 
-% fontsize = 20
-% linewidth = 3
-% set(0, "defaultlinelinewidth", linewidth);
-% figure('Position',[0,0,1500,2000]);
-% for i=1:4
-%     hold on;
-%     plot(parts * rows(train), stat_means(:, i), '-o', 'linewidth', linewidth);
-% end
-% set(gca, "linewidth", linewidth, "fontsize", fontsize);
-% legend({'width=0.0001', 'width=0.0005', 'width=0.001', 'width=0.002'});
-% xlabel('Training samples count');
-% ylabel('Classification error rate');
-% hold off;
-% grid on;
-% pause;
+disp('5. Parzen window width-----------------')
+parts = (1:5) / 5;
+parts = [0.06 0.08 parts]';
+stat_means = [];
+for i=1:rows(parts)
+    experiments_count = 20;
+    base_ercf = zeros(experiments_count,4);
+    for r=1:experiments_count
+        reduced = reduce(train, repmat(parts(i), rows(labels), 1));
+
+        para1 = para_parzen(reduced, 0.0001); 
+        para2 = para_parzen(reduced, 0.0005); 
+        para3 = para_parzen(reduced, 0.001); 
+        para4 = para_parzen(reduced, 0.002); 
+
+        base_ercf(r, 1) = mean(bayescls(test(:,2:end), @pdf_parzen, para1) != test(:,1));
+        base_ercf(r, 2) = mean(bayescls(test(:,2:end), @pdf_parzen, para2) != test(:,1));
+        base_ercf(r, 3) = mean(bayescls(test(:,2:end), @pdf_parzen, para3) != test(:,1));
+        base_ercf(r, 4) = mean(bayescls(test(:,2:end), @pdf_parzen, para4) != test(:,1));
+    end
+    stat_means = cat(1, stat_means, mean(base_ercf, 1));
+end
+
+fontsize = 20
+linewidth = 3
+set(0, "defaultlinelinewidth", linewidth);
+figure('Position',[0,0,1500,2000]);
+for i=1:4
+    hold on;
+    plot(parts * rows(train), stat_means(:, i), '-o', 'linewidth', linewidth);
+end
+set(gca, "linewidth", linewidth, "fontsize", fontsize);
+legend({'width=0.0001', 'width=0.0005', 'width=0.001', 'width=0.002'});
+xlabel('Training samples count');
+ylabel('Classification error rate');
+hold off;
+grid on;
+pause;
 
 
 
