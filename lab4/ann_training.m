010f914935cb766cf0e6b09c6a3cdee5029e5645
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-06-03 19:38:44 +0200

    Add initial experiment

--- lab4/ann_training.m
+++ lab4/ann_training.m
@@ -2,6 +2,9 @@
 tlab += 1;
 tstl += 1;
 
+% tvec = tvec(1:300, :);
+% tlab = tlab(1:300, :);
+
 % remove columns with zero std 
 toRemain = std(tvec) != 0;
 tvec = tvec(:, toRemain);
@@ -12,23 +15,22 @@ mu = mean(tvec);
 sig = std(tvec);
 
 noHiddenNeurons = 200;
-noEpochs = 5;
+noEpochs = 8;
 learningRate = 0.005;
 
-rand()
-rstate = rand("state");
-save rnd_state.txt rstate
-%load rndstate.txt 
-%rand("state", rndstate);
+rand ("seed", 123)
 
 % no normalization
 [hlnn olnn] = crann(columns(tvec), noHiddenNeurons, 10);
 trainError = zeros(1, noEpochs);
 testError = zeros(1, noEpochs);
 trReport = [];
+labels = unique(tstl)
+sumcfmx = cfmx = zeros(rows(labels), rows(labels) +1);
 for epoch=1:noEpochs
 	tic();
 	[hlnn olnn terr] = backprop(tvec, tlab, hlnn, olnn, learningRate);
+    learningRate = learningRate * 0.5
 	clsRes = anncls(tvec, hlnn, olnn);
 	cfmx = confMx(tlab, clsRes);
 	errcf = compErrors(cfmx);
@@ -36,6 +38,7 @@ for epoch=1:noEpochs
 
 	clsRes = anncls(tstv, hlnn, olnn);
 	cfmx = confMx(tstl, clsRes);
+    sumcfmx = sumcfmx + cfmx;
 	errcf2 = compErrors(cfmx);
 	testError(epoch) = errcf2(2);
 	epochTime = toc();
@@ -44,3 +47,8 @@ for epoch=1:noEpochs
 	[errcf errcf2]
 	fflush(stdout);
 end
+
+[(1:columns(testError))', testError']
+output_precision(0)
+[1:11 ; [sumcfmx / noEpochs]]
+assert(sum(sum(sumcfmx)) / noEpochs == rows(tstl))
