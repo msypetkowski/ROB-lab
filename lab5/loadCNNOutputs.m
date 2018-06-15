a0514036e32126a70631f35e90ff6eb2e3f53e6f
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-06-15 19:08:45 +0200

    Add simple experiment

--- lab5/loadCNNOutputs.m
+++ lab5/loadCNNOutputs.m
@@ -1,11 +1,21 @@
-function vtab = loadCNNOutputs(fnames)
+function vtab = loadCNNOutputs(fnames, probabilities=false)
 % Loads neural network outputs from specified files
 % Input files contain in each row output of the neural net for one row (sample) of a set
 % decmax function is used to reduce each row to index of maximum value the rows contains
 % vtab - output matrix containing one column for each file specified
  
 	vtab = [];
+    if probabilities
+        vtab = zeros(10000, 10);
+    end
 	for i=1:size(fnames,1)
 		valc = load(fnames{i});
-		vtab = [vtab decmax(valc)];
+        if probabilities
+            vtab = vtab + valc;
+        else
+            vtab = [vtab decmax(valc)];
+        end
 	end
+    if probabilities
+        vtab = vtab / rows(fnames);
+    end
---
+++ lab5/mainScript2.m
@@ -0,0 +1,40 @@
+vnames = {
+'valid_1.txt', 
+'valid_2.txt',
+'valid_3.txt', 
+'valid_4.txt', 
+'valid_5.txt', 
+'valid_6.txt', 
+'valid_7.txt'};
+
+tnames = {
+'test_1.txt', 
+'test_2.txt', 
+'test_3.txt', 
+'test_4.txt', 
+'test_5.txt', 
+'test_6.txt', 
+'test_7.txt'};
+	
+vtab = loadCNNOutputs(vnames, true);
+load validlab.txt
+validlab = validlab + 1;
+
+ttab = loadCNNOutputs(tnames, true);
+load testlab.txt
+testlab = testlab + 1;
+
+
+threshold = 0.79
+disp('------------------- simply thresholding mean probabilities')
+[ max_values answers ] = max(vtab, [], 2);
+answers(max_values < threshold) = 11;
+confmx = confMx(validlab, answers);
+errcfs = compErrors(confmx);
+resVald = [errcfs fobj(errcfs)]
+
+[ max_values answers ] = max(ttab, [], 2);
+answers(max_values < threshold) = 11;
+confmx = confMx(testlab, answers);
+errcfs = compErrors(confmx);
+resTest = [errcfs fobj(errcfs)]
